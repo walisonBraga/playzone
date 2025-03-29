989500c0fd3a27db57a609935cf2e764c6286af8
walisonbraga <walison.lemos@gmail.com>
2025-03-29 14:24:41 -0500

    Foi configurado com firebase o projeto PlayZone com angular.js

---
+++ src/app/shared/zona-user/zona-user.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-zona-user',
+  templateUrl: './zona-user.component.html',
+  styleUrls: ['./zona-user.component.css']
+})
+export class ZonaUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/shared/zona-user/zona-user.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ZonaUserComponent } from './zona-user.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ZonaUserComponent]
+})
+export class ZonaUserModule { }
